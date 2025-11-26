7737465fb396a4a67ff1aa1225b3db4c69cc19a3
UBC Student <cjim02@pender.students.cs.ubc.ca>
2025-11-25 21:00:18 -0800

    test commit

--- validation.js
+++ validation.js
@@ -49,6 +49,7 @@ function isValidPercentage(value) {
     return !isNaN(num) && num >= 0 && num <= 100;
 }
 
+// Exports (test commit)
 module.exports = {
     isPositiveInteger,
     isNonNegativeNumber,
